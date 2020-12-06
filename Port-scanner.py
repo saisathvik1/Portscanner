1a46208112ca26da7e752c98a130bccf90d2e094
Sai Sathvik Ruppa <51957422+SaiSathvik1@users.noreply.github.com>
2020-12-06 07:58:07 +0530

    Update Port-scanner.py

--- Port-scanner.py
+++ Port-scanner.py
@@ -5,6 +5,6 @@ scan_target = nmap.PortScanner()
 ports = [21,22,80,139,443,445,8080]
 
 for port in ports:
-  portscan = scan_target.scan(target,str(port))
+  portscanner = scan_target.scan(target,str(port))
   if portscanner!='closed':
     print(f"Port {port} on {target} is {portscan['scan'][target]['tcp'][port]['state']}")
