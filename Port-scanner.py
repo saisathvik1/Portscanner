7b4b03f8ba8c5aa1e3a122bad9de4e96b2e25413
Sai Sathvik Ruppa <51957422+SaiSathvik1@users.noreply.github.com>
2020-12-06 07:59:21 +0530

    Update Port-scanner.py

--- Port-scanner.py
+++ Port-scanner.py
@@ -7,4 +7,4 @@ ports = [21,22,80,139,443,445,8080]
 for port in ports:
   portscanner = scan_target.scan(target,str(port))
   if portscanner!='closed':
-    print(f"Port {port} on {target} is {portscan['scan'][target]['tcp'][port]['state']}")
+    print(f"Port {port} on {target} is {portscanner['scan'][target]['tcp'][port]['state']}")
