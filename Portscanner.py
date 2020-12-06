694f67ed57005514804f1d2b46956967e0048e7e
Sai Sathvik Ruppa <51957422+SaiSathvik1@users.noreply.github.com>
2020-12-06 07:51:01 +0530

    Update Portscanner.py

--- Portscanner.py
+++ Portscanner.py
@@ -1,1 +1,10 @@
 import nmap
+
+target = input("Enter Target IP: ")
+scan_target = nmap.PortScanner()
+ports = [21,22,80,139,443,445,8080]
+
+for port in ports:
+  portscan = scan_target.scan(target,str(port))
+  if portscanner!='closed':
+    print(f"Port {port} on {target} is {portscan['scan'][target]['tcp'][port]['state']}")
